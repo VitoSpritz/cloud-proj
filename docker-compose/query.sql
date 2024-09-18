ae29db3ddb3c1f9a0efd7ff447cc2c4be4055012
VitoSpritz <potenzavitopaolo@gmail.com>
2024-09-18 21:56:31 +0200

    Added some base code

--- docker-compose/query.sql
+++ docker-compose/query.sql
@@ -10,3 +10,11 @@ CREATE TABLE IF NOT EXISTS my_schema.contatti (
     gruppo VARCHAR(50),
     mail VARCHAR(100) UNIQUE
 );
+
+INSERT INTO my_schema.my_table (nome, cognome, telefono, citta, sesso, gruppo, mail)
+VALUES
+    ('Mario', 'Rossi', '1234567890', 'Roma', 'M', 'Admin', 'mario.rossi@example.com'),
+    ('Luigi', 'Verdi', '0987654321', 'Milano', 'M', 'User', 'luigi.verdi@example.com'),
+    ('Anna', 'Bianchi', '1112223334', 'Napoli', 'F', 'Admin', 'anna.bianchi@example.com'),
+    ('Carla', 'Gialli', '4445556667', 'Torino', 'F', 'User', 'carla.gialli@example.com'),
+    ('Marco', 'Neri', '7778889990', 'Firenze', 'M', 'Guest', 'marco.neri@example.com');
